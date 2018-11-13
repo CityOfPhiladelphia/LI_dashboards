72a89893203d6197ce141cdccca7b3994351262a
Philip Ribbens <Philip.Ribbens@phila.gov>
2018-11-13 10:59:50 -0500

    Added queries for individual workloads dashboards

---
+++ queries/Man001ActiveJobsBL.sql
@@ -0,0 +1,55 @@
+SELECT DISTINCT j.externalfilenum "JobNumber",
+  jt.description "JobType",
+  NVL(ap.licensetypesdisplayformat, ar.licensetypesdisplayformat) "LicenseType",
+  stat.description "JobStatus",
+  proc.processid "ProcessID",
+  pt.description "ProcessType",
+  Extract(MONTH FROM proc.datecompleted)
+  || '/'
+  ||Extract(DAY FROM proc.datecompleted)
+  || '/'
+  || Extract(YEAR FROM proc.datecompleted) "JobAcceptedDate",
+  proc.processstatus "ProcessStatus",
+  proc.assignedstaff "AssignedStaff",
+  (
+  CASE
+    WHEN ROUND(SYSDATE - proc.scheduledstartdate) <= 1
+    THEN '0-1 Day'
+    WHEN ROUND(SYSDATE - proc.scheduledstartdate) BETWEEN 2 AND 5
+    THEN '2-5 Days'
+    WHEN ROUND(SYSDATE - proc.scheduledstartdate) BETWEEN 6 AND 10
+    THEN '6-10 Days'
+    WHEN ROUND(SYSDATE - proc.scheduledstartdate) BETWEEN 11 AND 365
+    THEN '11 Days-1 Year'
+    ELSE 'Over 1 Year'
+  END) "TimeSinceScheduledStartDate",
+  (
+  CASE
+    WHEN jt.description LIKE 'Business License Application'
+    THEN 'https://eclipseprod.phila.gov/phillylmsprod/int/lms/Default.aspx#presentationId=1239699&objectHandle='
+      ||j.jobid
+      ||'&processHandle=&paneId=1239699_151'
+    WHEN jt.description LIKE 'Amendment/Renewal'
+    THEN 'https://eclipseprod.phila.gov/phillylmsprod/int/lms/Default.aspx#presentationId=1243107&objectHandle='
+      ||j.jobid
+      ||'&processHandle=&paneId=1243107_175'
+  END ) "JobLink"
+FROM api.jobs j,
+  api.jobtypes jt,
+  api.statuses stat,
+  api.processes PROC,
+  api.processtypes pt,
+  query.j_bl_amendrenew ar,
+  query.j_bl_application ap
+WHERE j.jobid          = proc.jobid
+AND proc.processtypeid = pt.processtypeid
+AND j.jobid            = ar.jobid (+)
+AND j.jobid            = ap.jobid (+)
+AND pt.processtypeid LIKE '1239327'
+AND proc.datecompleted IS NOT NULL
+AND j.jobtypeid         = jt.jobtypeid
+AND j.statusid          = stat.statusid
+AND j.completeddate    IS NULL
+AND j.jobtypeid        IN ( '1240320', '1244773' )
+AND j.statusid NOT     IN ( '1014809', '978845', '964970', '967394' )
+ORDER BY j.externalfilenum 
---
+++ queries/individual_workloads_bl.sql
@@ -0,0 +1,51 @@
+SELECT proc.processid,
+  pt.description ProcessType,
+  jt.description JobType,
+  ap.licensetypesdisplayformat LicenseType,
+  UPPER(u.name) name,
+  proc.processstatus,
+  proc.scheduledstartdate,
+  proc.datecompleted,
+  proc.datecompleted - proc.scheduledstartdate AS duration
+FROM api.processes PROC,
+  api.processtypes pt,
+  api.jobs j,
+  api.jobtypes jt,
+  query.j_bl_application ap,
+  api.users u
+WHERE proc.jobid           = j.jobid
+AND j.jobid                = ap.jobid
+AND proc.processtypeid     = pt.processtypeid
+AND j.jobtypeid            = jt.jobtypeid
+AND proc.completedbyuserid = u.userid
+AND proc.datecompleted     > '01-JAN-2018'
+AND proc.datecompleted    <= SYSDATE
+AND regexp_like(u.name, '[A-Za-z\s]+$')
+AND u.name <> 'PPG User'
+AND u.name <> 'POSSE system power user'
+UNION
+SELECT proc.processid,
+  pt.description ProcessType,
+  jt.description JobType,
+  ar.licensetypesdisplayformat LicenseType,
+  UPPER(u.name) name,
+  proc.processstatus,
+  proc.scheduledstartdate,
+  proc.datecompleted,
+  proc.datecompleted - proc.scheduledstartdate AS duration
+FROM api.processes PROC,
+  api.processtypes pt,
+  api.jobs j,
+  api.jobtypes jt,
+  query.j_bl_amendrenew ar,
+  api.users u
+WHERE proc.jobid           = j.jobid
+AND j.jobid                = ar.jobid
+AND proc.processtypeid     = pt.processtypeid
+AND j.jobtypeid            = jt.jobtypeid
+AND proc.completedbyuserid = u.userid
+AND proc.datecompleted     > '01-JAN-2018'
+AND proc.datecompleted    <= SYSDATE
+AND regexp_like(u.name, '[A-Za-z\s]+$')
+AND u.name <> 'PPG User'
+AND u.name <> 'POSSE system power user'
