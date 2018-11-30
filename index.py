0c7f6f9b61b09a9c755bafe74fc1d2a36b4a7bfb
Peter Dannemann <Peter.Dannemann@phila.gov>
2018-11-30 14:06:12 -0500

    Change email module name to prevent namespace collision

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ from flask import request
 from datetime import datetime
 
 from app import app, server
-from email import send_email
+from send_email import send_email
 #from log_visitors import log_visitor
 
 from apps import (Man001ActiveJobsBL, Man001ActiveJobsTL, Man002ActiveProcessesBL, Man002ActiveProcessesTL,
